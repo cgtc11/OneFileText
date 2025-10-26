d37d17a333f6b3a3142a13a668d1a00d48cb36fa
digimonkey <odc.cg.tc11@gmail.com>
2025-10-26 23:12:12 +0900

    プロジェクト ファイルを追加します。

---
+++ OneFileText/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace FileConcatenator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを解放する場合は true、それ以外の場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.fileListBox = new System.Windows.Forms.ListBox();
+            this.executeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // fileListBox
+            // 
+            this.fileListBox.AllowDrop = true;
+            this.fileListBox.FormattingEnabled = true;
+            this.fileListBox.ItemHeight = 12;
+            this.fileListBox.Location = new System.Drawing.Point(10, 10);
+            this.fileListBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.fileListBox.Name = "fileListBox";
+            this.fileListBox.Size = new System.Drawing.Size(620, 232);
+            this.fileListBox.TabIndex = 0;
+            // 
+            // executeButton
+            // 
+            this.executeButton.Location = new System.Drawing.Point(469, 246);
+            this.executeButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.executeButton.Name = "executeButton";
+            this.executeButton.Size = new System.Drawing.Size(161, 43);
+            this.executeButton.TabIndex = 1;
+            this.executeButton.Text = "実行";
+            this.executeButton.UseVisualStyleBackColor = true;
+            this.executeButton.Click += new System.EventHandler(this.ExecuteButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 300);
+            this.Controls.Add(this.executeButton);
+            this.Controls.Add(this.fileListBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "File Concatenator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox fileListBox;
+        private System.Windows.Forms.Button executeButton;
+    }
+}
---
+++ OneFileText/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FileConcatenator
+{
+    public partial class Form1 : Form
+    {
+        private SaveFileDialog saveFileDialog;
+        private bool isFileSaved = false; // ファイルが保存されたかどうかを示すフラグ
+
+        public Form1()
+        {
+            InitializeComponent();
+            fileListBox.AllowDrop = true;
+            fileListBox.DragEnter += new DragEventHandler(FileListBox_DragEnter);
+            fileListBox.DragDrop += new DragEventHandler(FileListBox_DragDrop);
+            fileListBox.MouseDown += new MouseEventHandler(FileListBox_MouseDown);
+            fileListBox.MouseMove += new MouseEventHandler(FileListBox_MouseMove);
+            executeButton.Click += new EventHandler(ExecuteButton_Click);
+
+            saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Text Files (*.txt)|*.txt",
+                Title = "保存先を選択してください"
+            };
+        }
+
+        private void FileListBox_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void FileListBox_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+                fileListBox.Items.Add(file);
+            }
+        }
+
+        private void ExecuteButton_Click(object sender, EventArgs e)
+        {
+            if (fileListBox.Items.Count == 0)
+            {
+                MessageBox.Show("ファイルを追加してください。", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!isFileSaved && saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string savePath = saveFileDialog.FileName;
+                ConcatenateFiles(savePath);
+                isFileSaved = true; // ファイルが保存されたことを示す
+            }
+        }
+
+        private void ConcatenateFiles(string savePath)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(savePath))
+                {
+                    foreach (string file in fileListBox.Items)
+                    {
+                        writer.WriteLine(Path.GetFileName(file));
+                        writer.WriteLine(new string('-', 50));
+                        writer.WriteLine(File.ReadAllText(file));
+                        writer.WriteLine(new string('-', 50));
+                    }
+                }
+                MessageBox.Show("ファイルを結合して保存しました。", "成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"エラーが発生しました: {ex.Message}", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private int dragIndex = -1;
+
+        private void FileListBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            dragIndex = fileListBox.IndexFromPoint(e.X, e.Y);
+        }
+
+        private void FileListBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && dragIndex != -1)
+            {
+                int newIndex = fileListBox.IndexFromPoint(e.X, e.Y);
+                if (newIndex != -1 && newIndex != dragIndex)
+                {
+                    object item = fileListBox.Items[dragIndex];
+                    fileListBox.Items.RemoveAt(dragIndex);
+                    fileListBox.Items.Insert(newIndex, item);
+                    dragIndex = newIndex;
+                }
+            }
+        }
+    }
+}
